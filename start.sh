90f50dc9eb8e4654e625cafd6d8436e47bebf9ef
Jaden - Jayhost <jadenhorst@gmail.com>
2018-02-13 16:20:19 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #java -Xms128M -Xmx128M -cp ".:/home/ian/craft/libs/*" DesktopLauncher
-java -cp ".:/home/ian/craft/libs/*:./classes:./assets/" de.swagner.paxbritannica.DesktopLauncher 
+java -cp "./android/*:./classes:./assets/" de.swagner.paxbritannica.DesktopLauncher 
 
